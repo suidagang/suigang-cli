a13be4f3275bc1683bcb34bfb95ccb7362fe34f5
suigang <suigang@css.com.cn>
2023-08-31 16:11:04 +0800

    fix: 获取项目路径修改

--- publish/utils/paths.js
+++ publish/utils/paths.js
@@ -1,4 +1,4 @@
 import path , { resolve } from 'path';
 const __dirname = path.resolve();
 //pkg根目录
-export const pkgPath = resolve(__dirname, '../../../');
+export const pkgPath = resolve(__dirname, '../../');
